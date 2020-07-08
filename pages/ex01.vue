5731786d4d22ba5e799564416a5f4be330f50fcf
benite1 <ita5931@ccollege.ac.th>
2020-07-08 16:05:21 +0700

    send1

---
+++ pages/ex01.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Hello {{ name }}</h2>
+    <h2>age {{ age }}</h2>
+    <input type="text" :value="name">
+    <input type="text" :v-model="name">
+    <div><button @click="dosave">SAVE</button></div>
+    <div><button @click="domenu">MAIN MENU</button></div>
+    <div><button @click="dohome">HOME</button></div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: 'Orasa Kartsanit',
+      age: 20,
+    }
+  },
+  methods: {
+    dosave() {
+      console.log('DO SAVE')
+    },
+    domenu() {
+      this.$router.push('/activity/menu')  
+    },
+    dohome() {
+      this.$router.push('/')
+    },
+  }
+}
+</script>
