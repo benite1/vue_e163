e8f6239b24f84ecb12d222a26d040e49ac4dc187
benite1 <ita5931@ccollege.ac.th>
2020-07-10 14:57:25 +0700

    login1

---
+++ pages/login.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-app>
+    <center>
+      <br><br>
+      <v-card width="1000" height="300" color="#726a95">
+        <v-card-title>
+          <h1>Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+            <v-text-field
+              prepend-icon="mdi-lock"
+              :append-icon="show3 ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="[rules.required, rules.min]"
+              :type="show3 ? 'text' : 'password'"
+              name="input-10-2"
+              label="Password"
+              hint="At least 8 characters"
+              value=""
+              class="input-group--focused"
+              @click:append="show3 = !show3"
+            />
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <nuxt-link to="register">
+            <v-btn large color="success" dark>
+              Regiter
+            </v-btn>
+          </nuxt-link>
+          <v-spacer />
+          <v-btn large color="purple" dark>
+            Login
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </center>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      show3: false,
+      password: 'Password',
+      rules: {
+        required: value => !!value || 'Required.',
+        min: v => v.length >= 8 || 'Min 8 characters',
+        emailMatch: () => ('The email and password you entered don\'t match'),
+      },
+    }
+  },
+}
+</script>
---
+++ pages/register.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-form v-model="valid">
+    <center>
+        <br><br>
+<v-card width="1200" color="#709fb0">
+      <br>
+      <center><h1 v-t="'Register'" /></center>
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="6">
+            <v-text-field label="name" />
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-text-field label="lastname" />
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-text-field label="username" />
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-text-field label="password" type="password" />
+          </v-col>
+        </v-row>
+        <v-btn
+          :disabled="!valid"
+          color="success"
+          class="mr-4"
+          @click="validate"
+        >
+          Submit
+          </v-btn>
+      </v-container>
+    </v-card>
+    </center>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: false,
+    firstname: '',
+    lastname: '',
+    nameRules: [
+      v => !!v || 'Name is required',
+      v => v.length <= 10 || 'Name must be less than 10 characters',
+    ],
+    email: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+/.test(v) || 'E-mail must be valid',
+    ],
+  }),
+}
+</script>
