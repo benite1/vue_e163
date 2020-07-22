2b3b209b5eb571f2a73ffd37f1b7c6b71676c0f2
benite1 <ita5931@ccollege.ac.th>
2020-07-22 15:35:59 +0700

    test2

---
+++ pages/ex02.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>HELLO {{ first }}age:{{std}} </h1>
+    <input type="text" :value="dep" color--text="red">
+     <v-text-field
+     v-model="first"
+     label="์NAME"
+            outlined
+            shaped
+          ></v-text-field>
+    
+    <v-text-field
+            solo
+            label="Name"
+            append-icon="mdi-map-marker"
+            clearable
+          ></v-text-field>
+
+          <v-text-field
+            v-model="last"
+            label="Last Name"
+            solo-inverted
+          ></v-text-field>
+
+          <v-text-field
+            v-model="title"
+            :rules="rules"
+            counter="20"
+            hint="This field uses counter prop"
+            label="Regular"
+            clearable
+          ></v-text-field>
+
+  </div>
+</template>
+<script>
+export default {
+  data:function(){
+    return{
+    name:"alongkorn",
+    age:45,
+    // dep:"information  technology",
+    first:"chaiyaphum"
+    }
+  }
+}
+</script>
